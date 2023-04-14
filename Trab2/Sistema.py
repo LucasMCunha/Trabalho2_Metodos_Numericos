4030b8e0798a4272720aad73a06e1d1d52e56854
LucasMCunha <90118707+LucasMCunha@users.noreply.github.com>
2023-04-13 22:26:31 -0300

    Add files via upload

---
+++ Trab2/Sistema.py
@@ -0,0 +1,38 @@
+
+def leitor():
+    #Recebe string inicial
+    #Devolve Matriz correspondente
+    return 0
+
+def gauss():
+    #matriz = A
+    #guard = false
+    #for i in range A
+    #skipper = 0
+    #for j in range i
+    #if(A[i][j] == 0)
+    # skipper+1
+    # skipper == 0 && guard == false
+    # troca a linha A[Skipper] com A[0]
+    #
+    # seta guard como true
+    # troca a linha A[Skipper] com A[i]
+    #
+    #
+    return 0
+
+def swap(A, i, j):
+    newI = []
+    newJ = []
+    for index in range(len(A) + 1):
+        newI += [A[j][index]]
+        newJ += [A[i][index]]
+    A[i] = newI
+    A[j] = newJ
+
+def main():
+    #Recebe entrada em string
+    #Transforma ela em matriz
+    #Aplica Gauss na matriz
+    #Gauss devolve X,Y e Z
+    return 0
